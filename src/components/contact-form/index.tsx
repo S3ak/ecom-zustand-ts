c4e670da65f6725e7c56453dfe099a35350ef117
Monde Sineke <nsineke@gmail.com>
2024-03-14 18:23:43 +0100

    feat: add validation

--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -1,15 +1,28 @@
 import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-type Inputs = {
-  name: string;
-  email: string;
-  message: string;
-};
+const contactFormSchema = yup
+  .object({
+    name: yup.string().min(2).max(20).required("Name is required"),
+    message: yup.string().min(2).max(100).required(),
+    email: yup.string().email().required(),
+  })
+  .required();
+
+type ContactFormInputs = yup.InferType<typeof contactFormSchema>;
 
 export default function ContactForm() {
-  const { register, handleSubmit, formState } = useForm<Inputs>();
+  const { register, handleSubmit, formState } = useForm<ContactFormInputs>({
+    resolver: yupResolver(contactFormSchema),
+  });
+  const { errors } = formState;
 
-  const onSubmit = ({ name, message, email }) => {
+  const onSubmit: SubmitHandler<ContactFormInputs> = ({
+    name,
+    message,
+    email,
+  }) => {
     fetch("https://dummyjson.com/posts/add", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -24,29 +37,71 @@ export default function ContactForm() {
   };
 
   return (
-    <div>
+    <div className="w-full">
       <h2>Contact Form</h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input {...register("name", { required: true })} type="text" />
-          {formState.errors.name && <span>This field is required</span>}
-        </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md"
+      >
+        <fieldset className="flex flex-col gap-2">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-bold text-gray-700"
+            >
+              Name
+            </label>
+            <input
+              {...register("name")}
+              type="text"
+              aria-label="name"
+              aria-invalid={errors.name ? "true" : "false"}
+              aria-placeholder="John Smith"
+              placeholder="John Smith"
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            />
+            {errors.name && <span>This field is required</span>}
+          </div>
 
-        <div>
-          <label htmlFor="email">Email</label>
-          <input {...register("email")} type="email" />
-        </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-bold text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              {...register("email")}
+              type="email"
+              aria-placeholder="mo@mail.com"
+              placeholder="mo@mail.com"
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            />
+          </div>
 
-        <div>
-          <label htmlFor="message">Message</label>
-          <textarea {...register("message")}></textarea>
-        </div>
+          <div>
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-bold text-gray-700"
+            >
+              Message
+            </label>
+            <textarea
+              {...register("message")}
+              aria-placeholder="..."
+              placeholder="hello world..."
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+            ></textarea>
+          </div>
+        </fieldset>
         <br />
-        <hr />
-        <button type="submit">Send</button>
-        <hr />
+        <button
+          type="submit"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+        >
+          Send
+        </button>
       </form>
     </div>
   );
